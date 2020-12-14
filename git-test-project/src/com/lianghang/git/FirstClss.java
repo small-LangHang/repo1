e0c5b142273c23a1fdb3bfb1076b455228f86025
small_LiangHang <email_lianghang@126.com>
2020-12-14 21:01:47 +0800

    测试git

---
+++ git-test-project/src/com/lianghang/git/FirstClss.java
@@ -0,0 +1,16 @@
+package com.lianghang.git;
+
+/**
+ * @Author: LiangHang
+ * @date 2020/12/14
+ */
+public class FirstClss {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+    }
+}
