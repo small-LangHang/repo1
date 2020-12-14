a6725b01ccd21c3d72ee5ecbf3b09eac29099b79
small_LiangHang <email_lianghang@126.com>
2020-12-14 21:08:38 +0800

    修改了东西

--- git-test-project/src/com/lianghang/git/FirstClss.java
+++ git-test-project/src/com/lianghang/git/FirstClss.java
@@ -5,12 +5,33 @@ package com.lianghang.git;
  * @date 2020/12/14
  */
 public class FirstClss {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public static void main(String[] args) {
+
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("Hello Git");
+
     }
 }
